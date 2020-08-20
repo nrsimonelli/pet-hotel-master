0f48798bca3170bc667e0bbc836ac4937c5e95a0
Ross Hutchens <rlhcxm@gmail.com>
2020-08-20 12:38:11 -0500

    Connected to database and returning connection records

--- main.py
+++ main.py
@@ -3,6 +3,50 @@ from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
+# import psycopg2 to use Python with PostgreSQL
+import psycopg2
+# assign database name & location to variable "db"
+db = "dbname=%s host=%s " % ('pet_hotel', 'localhost')
+#  ???
+schema = "schema.sql"
+# initializes connection to PostgreSQL
+def get_connection():
+    connection = psycopg2.connect(db)
+    return connection
+
+# closes connection to PostgreSQL
+# def close_connection(connection):
+#     if connection:
+#         connection.close()
+#         print("Postgres connection is now closed")
+
+# prints database version
+def read_database_version():
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute("SELECT version();")
+        db_version = cursor.fetchone()
+        print("You are connect to PostgreSQL version: ", db_version)
+        # close_connection(connection)
+    except (Exception, psycopg2.Error) as error:
+        print("Error while getting data", error)
+
+print("Question 1: Print Database version")
+read_database_version()
+
+# function to get rows from table "pet"
+# try:
+#     connection = psycopg2.connect(db)
+#     cursor = connection.cursor()
+#     select_query = "SELECT * FROM owner JOIN pet ON owner.id = pet.owner_id ORDER BY pet.id;"
+
+#     cursor.execute(select_query)
+#     print("Selecting all from pets")
+
+# except (Exception, psycopg2.Error) as error :
+#     print ("Error while connecting to PostgreSQL", error)
+# end try catch
 
 # creating dictionary
 pets = [{"pet": "birdo", "owner": "max", "breed": "parakeet", "color": "blue", "check-in": "no" },
