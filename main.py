41937c8b632cf48e464ce625ff0460eb720971d8
Ross Hutchens <rlhcxm@gmail.com>
2020-08-20 14:24:31 -0500

    Delete route working

--- main.py
+++ main.py
@@ -50,13 +50,38 @@ def select_all_pets():
             print("Animal Breed: ", row[5])
             print("Color: ", row[6])
             print("Checked In: ", row[7])
-        close_connection(connection)
+        return records
     except (Exception, psycopg2.Error) as error:
         print("Error getting pets", error)
+    finally:
+        close_connection(connection)
 
-# runs function to get all rows from pet table
-print("Getting all pets:")
-select_all_pets()
+def insert_new_pet(pet):
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        sql_insert_query = """INSERT INTO pet (owner_id, pet, breed, color, check_in) VALUES 
+	(%s, %s, %s, %s, %s);"""
+        cursor.execute(sql_insert_query, (pet["owner-id"], pet["pet"], pet["breed"], pet["color"], pet["check-in"]))
+        connection.commit()
+        close_connection(connection)
+    except (Exception, psycopg2.Error) as error:
+        print("Error while getting doctor's data", error)
+
+# runs function to get all rows from pet table when server is spun up
+# print("Getting all pets:")
+# select_all_pets()
+
+def delete_pet(id):
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        sql_delete_query = """DELETE FROM pet WHERE pet.id=%s;"""
+        cursor.execute(sql_delete_query, (id,))
+        connection.commit()
+        close_connection(connection)
+    except (Exception, psycopg2.Error) as error:
+        print("Error deleting pet from database", error)
 
 
 # creating dictionary
@@ -70,24 +95,23 @@ class PetHotel(Resource):
 # defining GET
   def get(self):
     # setting return with KEY and VALUE
-    return pets
+    return select_all_pets()
   
   def post(self):
-    pets.append({"pet": "dexter", "owner": "nick", "breed": "robot", "color": "purple", "check-in": "yes" })
+    # pets.append({"pet": "dexter", "owner": "nick", "breed": "robot", "color": "purple", "check-in": "yes" })
+    pet = {"pet": "dexter", "owner-id": 1, "breed": "robot", "color": "purple", "check-in": "yes" }
+    insert_new_pet(pet)
     return 201
 
-  def delete(self):
-    if len(pets) > 0:    
-      del pets[len(pets)-1]
-      
+  def delete(self, id):
+    delete_pet(id)
     return '', 204 
 
   def patch(self):
     pets[0] = {"pet": 'TEST'}
-
     return 201   
    
-api.add_resource(PetHotel, "/")
+api.add_resource(PetHotel, "/<int:id>")
 
 # @app.route("/")
 
